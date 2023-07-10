404d8240f3b4f2f14a3bd3623935d9c2ff1edb0f
Md. Mehedi Hasan <m.hasanjoy13@gmail.com>
2023-07-10 20:02:26 +0600

    [DONE] setup local database

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.euxm4cs.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.euxm4cs.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb://127.0.0.1:27017/`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
